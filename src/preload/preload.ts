01b0a97776efc65c95f6004c6aa6fc3c68b06a1a
yosle <yosle007@gmail.com>
2023-07-02 04:32:19 -0400

    linting

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -2,8 +2,6 @@
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 import {ipcRenderer, contextBridge } from "electron";
 
-
-
 // contextBridge.exposeInMainWorld('electronAPI', {
 //     setTitle: (title: string) => {
 //     return ipcRenderer.send('set-title', title);
@@ -22,7 +20,7 @@ contextBridge.exposeInMainWorld('electronAPI', {
 
 ipcRenderer.on('asynchronous-reply', (_event, arg) => {
   console.log(arg) // prints "pong" in the DevTools console
+  const result = ipcRenderer.sendSync('synchronous-message', 'ping')
+console.log(result) // prints "pong" in the DevTools console
 })
 
-const result = ipcRenderer.sendSync('synchronous-message', 'ping')
-console.log(result) // prints "pong" in the DevTools console
