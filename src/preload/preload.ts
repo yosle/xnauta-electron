da1f88034803f7ea7c5e5f7232c7b51bc16cbd9b
yosle <yosle007@gmail.com>
2023-07-05 11:37:18 -0400

    add ipc events

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,6 +1,6 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-import {ipcRenderer, contextBridge } from "electron";
+import { ipcRenderer, contextBridge, IpcRendererEvent, IpcMainEvent } from "electron";
 
 // contextBridge.exposeInMainWorld('electronAPI', {
 //     setTitle: (title: string) => {
@@ -9,18 +9,22 @@ import {ipcRenderer, contextBridge } from "electron";
 // });
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    handleLogin: (user: string, password: string) => {
+  handleLogin: (user: string, password: string) => {
     return ipcRenderer.send('on-handleLogin', user, password);
-  }
+  },
+  handleCounter: (callback: (event: IpcRendererEvent | IpcMainEvent, value: any) => void) => ipcRenderer.on('show-counter', callback)
+
 });
 
-contextBridge.exposeInMainWorld('electronAPI', {
-  openFile: () => ipcRenderer.invoke('dialog:openFile')
-});
 
-ipcRenderer.on('asynchronous-reply', (_event, arg) => {
-  console.log(arg) // prints "pong" in the DevTools console
-  const result = ipcRenderer.sendSync('synchronous-message', 'ping')
-console.log(result) // prints "pong" in the DevTools console
-})
+
+// contextBridge.exposeInMainWorld('electronAPI', {
+// })
+
+
+
+
+
+
+
 
